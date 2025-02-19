bb4e164f27f5f9472a93b90d8834eafe26da6441
Sloane-J <samueldorkeyjr@gmail.com>
2025-02-19 14:33:09 +0000

    added inital navigation and hero section code.

---
+++ portfolio/tailwind.config.mjs
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", "*.{js,ts,jsx,tsx,mdx}"],
+    darkMode: 'class',
+    theme: {
+      extend: {
+        colors: {
+          background: 'rgb(var(--background))',
+          card: 'rgb(var(--card))',
+          border: 'rgb(var(--border))',
+          primary: 'rgb(var(--primary))',
+          muted: 'rgb(var(--muted))',
+        },
+        animation: {
+          'gradient': 'gradient 8s linear infinite',
+        },
+        keyframes: {
+          gradient: {
+            '0%, 100%': {
+              'background-size': '200% 200%',
+              'background-position': 'left center',
+            },
+            '50%': {
+              'background-size': '200% 200%',
+              'background-position': 'right center',
+            },
+          },
+        },
+      },
+    },
+    plugins: [],
+  }
+  
+  
